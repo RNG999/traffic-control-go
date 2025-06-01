26d2e766426b4e50e52b92a5d27e030b0cdbd394
thetaru <sensesensesense012345678910@gmail.com>
2025-06-01 14:58:52 +0000

    fix: move nosec comments to separate lines for proper gosec recognition

--- api/traffic_control.go
+++ api/traffic_control.go
@@ -469,7 +469,8 @@ func (tc *TrafficController) Apply() error {
 			if j > 65435 { // Prevent overflow when adding 100 (65535 - 100)
 				return fmt.Errorf("too many filters: would overflow uint16")
 			}
-			priority := uint16(100) + uint16(j) // Start filter priorities at 100 #nosec G115 -- overflow check performed above
+			// #nosec G115 -- overflow check performed above
+			priority := uint16(100) + uint16(j) // Start filter priorities at 100
 			protocol := "ip"
 			flowID := classID
 
